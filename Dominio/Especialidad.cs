029e1af71a6f5519abdd1b1d9f0bf4224d5702bb
Caeli1010 <elizabeth.ayala4@gmail.com>
2021-06-25 21:41:48 -0300

    Remendando la macana de Eugenio, por no dejarme hacer el push.

--- Dominio/Especialidad.cs
+++ Dominio/Especialidad.cs
@@ -10,6 +10,16 @@ namespace Dominio
     {
         public int idEspecialidad { get; set; }
         public string nombre { get; set; }
+        public int convenio { get; set; }
+        public Especialidad()
+        {
+
+        }
+        public Especialidad(int id, string _nombre)
+        {
+            idEspecialidad = id;
+            nombre = _nombre;
+        }
         public override string ToString()
         {
             return nombre;
---
+++ Negocio/EspecialidadNegocio.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    class EspecialidadNegocio
+    {
+        public List<Especialidad> listar()
+        {
+            List<Especialidad> lista = new List<Especialidad>();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta(@"SELECT IDESPECIALIDAD, NOMBRE FROM ESPECIALIDADES");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Especialidad aux = new Especialidad();
+                    aux.idEspecialidad = (int)datos.Lector["IDESPECIALIDAD"];
+                    aux.nombre = (string)datos.Lector["NOMBRE"];
+                    lista.Add(aux);
+
+                }
+                return lista;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+        public void agregar(Especialidad nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                string valores = @"values('" + nuevo.nombre + "')";
+                datos.setearConsulta(@"insert into ESPECIALIDADES ( NOMBRE ) " + valores);
+                datos.ejecutarAccion();
+
+            }
+            catch (global::System.Exception)
+            {
+                throw;
+            }
+
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+        public void modificar(Especialidad modificar)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("update EspecialidadS set NOMBRE = @nombre");
+                datos.setearParametro("@nombre", modificar.nombre);
+                datos.ejecutarAccion();
+
+            }
+            catch (global::System.Exception)
+            {
+                throw;
+            }
+
+            finally
+            {
+                datos.cerrarConexion();
+                datos = null;
+            }
+        }
+      
+        public void leerEspecialidad(string nombre)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("select * from ESPECIALIDADES where NOMBRE like " + nombre  + "");
+                datos.ejecutarLectura();
+
+            }
+            catch (global::System.Exception)
+            {
+                throw;
+            }
+
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }
+    }
+}
