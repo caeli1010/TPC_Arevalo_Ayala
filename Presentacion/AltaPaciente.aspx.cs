bc87eeff8c31d6ac64353a337c65abda35f30dc0
Eugenio <reugenioarevalo@gmail.com>
2021-07-03 00:01:50 -0300

    alta paciente solo front

--- Presentacion/AltaPaciente.aspx.cs
+++ Presentacion/AltaPaciente.aspx.cs
@@ -9,12 +9,19 @@ namespace Presentacion
 {
     public partial class AltaPaciente : System.Web.UI.Page
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
             //if (Session["Login"] == null)
             //{
             //    Response.Redirect("Login2.aspx");
             //}
+
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+
 
         }
     }
